d78e8589973fc3c73c3b2586e39b2e3801c692d8
Brolocuelo <luisalfonsorello@gmail.com>
2023-01-24 13:53:08 +0100

    Progress

---
+++ Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public bool gameOver;
+
+    public float floatForce;
+    private float gravityModifier = 1f;
+    private Rigidbody playerRb;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem fireworksParticle;
+
+    private AudioSource playerAudio;
+    public AudioClip moneySound;
+    public AudioClip explodeSound;
+
+
+    void Start()
+    {
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>();
+
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * floatForce , ForceMode.Impulse);
+        }
+    }
+
+    private void GameOver()
+    {
+        gameOver = true;
+        explosionParticle.Play();
+        playerAudio.PlayOneShot(explodeSound, 1);
+        Debug.Log("Game Over!");
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Bomb"))
+        {
+            GameOver();
+            Destroy(other.gameObject);
+            if (!gameOver)
+            {
+                explosionParticle.Play();
+            }
+        } 
+
+
+        else if (other.gameObject.CompareTag("Money"))
+        {
+            fireworksParticle.Play();
+            playerAudio.PlayOneShot(moneySound, 1);
+            Destroy(other.gameObject);
+        }
+
+    }
+
+}
